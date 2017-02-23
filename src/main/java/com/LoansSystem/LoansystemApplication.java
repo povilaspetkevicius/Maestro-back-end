704af7d893789baabd25bb4bee43b4cbe3a6ca33
Povilas <p.petkevicius200@gmail.com>
2017-02-23 17:27:32 +0200

    [dev] Works on Port 8080, still working on heroku implementation.

--- src/main/java/com/LoansSystem/LoansystemApplication.java
+++ src/main/java/com/LoansSystem/LoansystemApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LoansystemApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(LoansystemApplication.class, args);
 	}
---
+++ src/main/java/com/LoansSystem/databaseconfig/ControlBean.java
@@ -0,0 +1,7 @@
+package com.LoansSystem.databaseconfig;
+
+/**
+ * Created by pov on 17.2.23.
+ */
+public class ControlBean {
+}
