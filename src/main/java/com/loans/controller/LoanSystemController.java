bf8a5dec257010293d7887838865bf1424595470
Povilas <p.petkevicius200@gmail.com>
2017-02-27 13:56:50 +0200

    new data insertion

--- src/main/java/com/loans/controller/LoanSystemController.java
+++ src/main/java/com/loans/controller/LoanSystemController.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 /**
  * Created by pov on 17.2.23.
  */
-@CrossOrigin(origins = "https://maestro-backend.herokuapp.com/")
+//@CrossOrigin(origins = "https://maestro-backend.herokuapp.com/")
 @RestController
 public class LoanSystemController {
 
